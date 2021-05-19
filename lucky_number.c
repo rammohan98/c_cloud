327e3337b783d5604bd2b2c3ed7a649d4be19edf
rammohan98 <84376652+rammohan98@users.noreply.github.com>
2021-05-19 10:54:51 +0530

    Add files via upload

---
+++ lucky_number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int n,d,s,x;
+	scanf("%d",&n);    
+	s=0;
+	while(n)
+	{  
+		d=n%10; 
+		n=n/10;    
+		s=s+d;    
+	}
+	if(s<9)
+	{
+		printf("%d",s);     
+	}
+	else
+	{
+		while(s>9)
+		{
+			n=s;
+			s=0;
+			while(n)
+			{
+				x=n%10;  
+				n=n/10;   
+				s=s+x;
+			}
+		}
+		printf("%d",s);
+	}
+	return 0;
+}
